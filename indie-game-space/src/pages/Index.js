f607a86dc80a99e69b5f6508cb70a487aff0fd8e
tellemorales <62278479+tellemorales@users.noreply.github.com>
2022-03-12 01:34:51 +0800

    AdminTest

--- indie-game-space/src/pages/Index.js
+++ indie-game-space/src/pages/Index.js
@@ -4,4 +4,5 @@
 export {default as Developers} from './Developers';
 export {default as DevsSignIn} from './DevsSignIn';
 export {default as DevsSignUp} from './DevsSignUp';
+
 export {default as Games} from './Games';
